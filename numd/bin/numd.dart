d97971000e0e93a2b15964e7bceee0b13053871f
Stephan Philips <s.g.j.philips@tudelft.nl>
2023-05-22 09:48:55 +0200

    cleaning

--- numd/bin/numd.dart
+++ numd/bin/numd.dart
@@ -1,10 +1,9 @@
 import 'package:numd/numd.dart' as nd;
-import 'package:numd/src/base/view.dart';
 
 void main(List<String> args) {
   var a = nd.ones([1000, 1000]);
 
-  // print(d);
+  print(a);
 //   var b = a[0];
 //   print(b[0]);
 //   print(b.view);
