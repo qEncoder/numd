74921f3de66c71577b603f36b115369286a27fc1
Stephan Philips <s.g.j.philips@tudelft.nl>
2024-03-12 20:17:03 +0100

    update build

--- numd/linux/flutter/generated_plugin_registrant.cc
+++ numd/linux/flutter/generated_plugin_registrant.cc
@@ -8,10 +8,4 @@
 
 
 void fl_register_plugins(FlPluginRegistry* registry) {
-#include <numd_c_libs/numd_c_libs_plugin.h>
-
-void fl_register_plugins(FlPluginRegistry* registry) {
-  g_autoptr(FlPluginRegistrar) numd_c_libs_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "NumdCLibsPlugin");
-  numd_c_libs_plugin_register_with_registrar(numd_c_libs_registrar);
 }
